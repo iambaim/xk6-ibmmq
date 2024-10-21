6bd19655720fb9e1db0c7173a5981f3224ec18eb
Ibrahim Umar <iambaim@users.noreply.github.com>
2024-10-21 15:50:02 +0200

    Enable user to specify arbitrary properties

--- example/localtest.js
+++ example/localtest.js
@@ -7,11 +7,15 @@ export default function () {
     const replyQueue = "DEV.QUEUE.2"
     const sourceMessage = "Sent Message"
     const replyMessage = "Reply Message"
+    const extraProperties = new Map([
+        ["apiVersion", 2],
+        ["extraText", "extra"]
+    ])
     // The below parameter enable/disable a simulated application that will consume
     // the message in the source queue and put a reply message in the reply queue
     // and the reply message correlation ID == source message ID
     const simulateReply = true
 
-    const msgId = ibmmq.send(sourceQueue, replyQueue, sourceMessage, simulateReply)
+    const msgId = ibmmq.send(sourceQueue, replyQueue, sourceMessage, extraProperties, simulateReply)
     ibmmq.receive(replyQueue, msgId, replyMessage)
 }
