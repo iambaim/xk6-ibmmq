5ef629f8d45d6fccb8d3e8c72c42b7e50233738e
Ibrahim Umar <iambaim@users.noreply.github.com>
2026-06-30 21:38:20 +0200

    Fix unbound variable again

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [[ ! -z ${GITHUB_RUN_ID+y} ]]; then
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     export CGO_CFLAGS="-I$MQ_INSTALLATION_PATH/inc"
     export CGO_LDFLAGS="-L$MQ_INSTALLATION_PATH/lib64 -Wl,-rpath,$MQ_INSTALLATION_PATH/lib64"
-  elif [[ "$OSTYPE" == "msys" ]]; then
+  elif [[ "$OSTYPE" == msys* || "$OSTYPE" == mingw* ]]; then
     export CGO_CFLAGS="-I$MQ_INSTALLATION_PATH/tools/c/include -D_WIN64"
     export CGO_LDFLAGS="-L$MQ_INSTALLATION_PATH/bin64 -lmqm"
   fi
@@ -18,5 +18,5 @@ if [[ ! -z ${GITHUB_RUN_ID+y} ]]; then
 fi
 
 go install go.k6.io/xk6/cmd/xk6@latest
-XK6_RACE_DETECTOR=1 GCO_ENABLED=1 xk6 -v build \
+XK6_RACE_DETECTOR=1 CGO_ENABLED=1 xk6 -v build \
     --with github.com/iambaim/xk6-ibmmq=.
