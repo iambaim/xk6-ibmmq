62c8e4f8466edd2f642c885f6ccc32049baad3db
Ibrahim Umar <iambaim@users.noreply.github.com>
2026-06-30 23:05:08 +0200

    Fix/build sh unbound variable (#51)
    * Fix unbound variable in build.sh
    
    * Fix unbound variable again
    
    * Debug OSTYPE
    
    * Fix variables

--- build.sh
+++ build.sh
@@ -6,17 +6,18 @@ set -euo pipefail
 # For github actions
 if [[ ! -z ${GITHUB_RUN_ID+y} ]]; then
   export MQ_INSTALLATION_PATH=$HOME/IBM/MQ/data
+  echo "OSTYPE=$OSTYPE"
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     export CGO_CFLAGS="-I$MQ_INSTALLATION_PATH/inc"
     export CGO_LDFLAGS="-L$MQ_INSTALLATION_PATH/lib64 -Wl,-rpath,$MQ_INSTALLATION_PATH/lib64"
-  elif [[ "$OSTYPE" == "msys" ]]; then
+  elif [[ "$OSTYPE" == msys* || "$OSTYPE" == mingw* || "$OSTYPE" == cygwin* ]]; then
     export CGO_CFLAGS="-I$MQ_INSTALLATION_PATH/tools/c/include -D_WIN64"
     export CGO_LDFLAGS="-L$MQ_INSTALLATION_PATH/bin64 -lmqm"
   fi
   echo $MQ_INSTALLATION_PATH
-  echo $CGO_LDFLAGS
+  echo "${CGO_LDFLAGS-}"
 fi
 
 go install go.k6.io/xk6/cmd/xk6@latest
-XK6_RACE_DETECTOR=1 GCO_ENABLED=1 xk6 -v build \
+XK6_RACE_DETECTOR=1 CGO_ENABLED=1 xk6 -v build \
     --with github.com/iambaim/xk6-ibmmq=.
